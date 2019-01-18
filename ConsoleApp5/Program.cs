699541a90a72e91ce3961dc4c3765c55ef0c3aa6
Sri Vidya <poshala.srividya@gmail.com>
2019-01-18 11:16:35 +0530

    hello world

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
